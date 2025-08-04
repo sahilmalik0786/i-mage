7469fce353fc907c7252996ae5d3fc013876f5dd
sahilmalik0786 <rahisu78@gmail.com>
2025-08-04 18:32:12 +0530

    updated the mongoose connect uri

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 function connectToDb(){
-    mongoose.connect('mongodb://localhost:27017/captiongenerator')
+    mongoose.connect(process.env.MONGODB_URI)
     .then(()=>{
         console.log('connected to db')
     })
